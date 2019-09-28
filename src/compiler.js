2ef363fed061db69b2d601a5884378724f2aa551
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-09-28 01:44:21 -0400

    fix(compiler): allow referencing viewof and mutable cells
    fancier test
    
    simplify walk
    
    tweak comments

--- src/compiler.js
+++ src/compiler.js
@@ -1,8 +1,10 @@
-import { parseCell, parseModule } from "@observablehq/parser";
+import { parseCell, parseModule, walk } from "@observablehq/parser";
 import { Library } from "@observablehq/runtime";
 import { extractPath } from "./utils";
+import { simple } from 'acorn-walk';
 
-const { Generators } = new Library();
+const { Generators, Mutable: constantMutable } = new Library();
+const Mutable = constantMutable();
 
 const AsyncFunction = Object.getPrototypeOf(async function() {}).constructor;
 const GeneratorFunction = Object.getPrototypeOf(function*() {}).constructor;
@@ -18,17 +20,52 @@ const createRegularCellDefintion = cell => {
   let name = null;
   if (cell.id && cell.id.name) name = cell.id.name;
   else if (cell.id && cell.id.id && cell.id.id.name) name = cell.id.id.name;
-  const bodyText = cell.input.substring(cell.body.start, cell.body.end);
+  let bodyText = cell.input.substring(cell.body.start, cell.body.end);
+  const cellReferences = (cell.references || []).map(ref => {
+    if (ref.type === "ViewExpression") {
+      return 'viewof ' + ref.id.name;
+    } else if (ref.type === "MutableExpression") {
+      return 'mutable ' + ref.id.name;
+    } else return ref.name;
+  });
+  let $count = 0;
+  let indexShift = 0;
+  const references = (cell.references || []).map(ref => {
+    if (ref.type === "ViewExpression") {
+      const $string = '$' + $count;
+      $count++;
+      // replace "viewof X" in bodyText with "$($count)"
+      simple(cell.body, {
+        ViewExpression(node) {
+          const start = node.start - cell.body.start;
+          const end = node.end - cell.body.start;
+          bodyText = bodyText.slice(0, start + indexShift) + $string + bodyText.slice(end + indexShift);
+          indexShift += $string.length - (end - start);
+        }
+      }, walk);
+      return $string;
+    } else if (ref.type === "MutableExpression") {
+      const $string = '$' + $count;
+      const $stringValue = $string + '.value';
+      $count++;
+      // replace "mutable Y" in bodyText with "$($count).value"
+      simple(cell.body, {
+        MutableExpression(node) {
+          const start = node.start - cell.body.start;
+          const end = node.end - cell.body.start;
+          bodyText = bodyText.slice(0, start + indexShift) + $stringValue + bodyText.slice(end + indexShift);
+          indexShift += $stringValue.length - (end - start);
+        }
+      }, walk);
+      return $string;
+    } else return ref.name;
+  });
   let code;
   if (cell.body.type !== "BlockStatement") {
     if (cell.async)
       code = `return (async function(){ return (${bodyText});})()`;
     else code = `return (function(){ return (${bodyText});})()`;
   } else code = bodyText;
-  const references = (cell.references || []).map(ref => {
-    if (ref.type === "ViewExpression") throw Error("ViewExpression wat");
-    return ref.name;
-  });
 
   let f;
 
@@ -40,7 +77,7 @@ const createRegularCellDefintion = cell => {
   return {
     cellName: name,
     cellFunction: f,
-    cellReferences: references
+    cellReferences
   };
 };
 const createModuleDefintion = (m, resolveModule) => {
@@ -96,7 +133,15 @@ const createModuleDefintion = (m, resolveModule) => {
           main
             .variable(observer(reference))
             .define(reference, cellReferences, cellFunction);
-          main.variable(null).define(cellName, [reference], Generators.input);
+          main.variable(observer(cellName)).define(cellName, [reference], Generators.input);
+        } else if (cell.id && cell.id.type === "MutableExpression") {
+          const initialName = `initial ${cellName}`;
+          const mutableName = `mutable ${cellName}`;
+          main
+            .variable(null)
+            .define(initialName, cellReferences, cellFunction);
+          main.variable(observer(mutableName)).define(mutableName, [initialName], (_) => new Mutable(_));
+          main.variable(observer(cellName)).define(cellName, [mutableName], _ => _.generator);
         } else {
           main
             .variable(observer(cellName))
