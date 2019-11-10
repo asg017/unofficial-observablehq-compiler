9a96569c1a6ec7b30965edea991632be487e243f
Bryan Gin-ge Chen <bryangingechen@gmail.com>
2019-11-10 15:54:30 -0500

    rename exec_test.js to executable_attachment.js

--- test/executable_attachment.js
+++ test/executable_attachment.js
@@ -1,4 +1,4 @@
 // https://observablehq.com/@jashkenas/executable-js-fileattachment-test
 export const html = document.createElement("H1");
 
-html.innerHTML = "<marquee>Successfully executed attachment: exec_test.js!</marquee>";
+html.innerHTML = "<marquee>Successfully executed JS in attachment!</marquee>";
